15107accc35ee89a2a5fa7026279c24a22bd5cde
Juan Sebastian Betancur Posada <jbetancu@bancolombia.com.co>
2022-08-24 09:26:49 -0500

    crud-initialized

--- controllers/alumnos.js
+++ controllers/alumnos.js
@@ -45,52 +45,60 @@ let controller = {
         }
 
         let user_info = req.body;
-        console.log(user_info);
 
-        // let alumnos_model = new AlumnosModel();
+        Alumnos.findOne({n_cuenta: user_info.n_cuenta}).exec( (err, alumno) => {
+            if(err) return res.status(500).json({status: 500, message: err});
+            if(alumno) return res.status(200).json({status: 200, message:"El numero de cuenta ya existe."});
 
-        // alumnos_model.n_cuenta = new_alumno.n_cuenta;
-        // alumnos_model.nombre = new_alumno.nombre;
-        // alumnos_model.edad = new_alumno.edad;
-        // alumnos_model.genero = new_alumno.genero;
+            let alumnos_model = new Alumnos();
 
-        // alumnos_model.save((err, alumnoStored) => {
-        //     if(err) return res.status(500).json({message:"Error en el proceso de creación"});
-        //     if(!alumnoStored) return res.status(200).json({message:"No se almacenó el alumno."})
+            alumnos_model.n_cuenta = user_info.n_cuenta;
+            alumnos_model.nombre = user_info.nombre;
+            alumnos_model.edad = user_info.edad;
+            alumnos_model.genero = user_info.genero;
 
-        //     return res.status(200).json({
-        //         message: "Alumno registrado"
-        //     });
-        // });
-    }
+            alumnos_model.save((err, alumnoStored) => {
+                if(err) return res.status(500).json({status: 500, message:"Error en el proceso de creación"});
+                if(!alumnoStored) return res.status(200).json({status: 200, message:"No se almacenó el alumno."})
 
-    // },
+                return res.status(200).json({
+                    status: 200,
+                    message: "Alumno registrado"
+                });
+            });
+        });
+    },
 
-    // update_alumno: function(req, res) {
+    update_alumno: function(req, res) {
+        //Validamos los datos que se envian al endpoint
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
 
-    //     let update_alumno = req.body;
-    //     let n_cuenta = req.params.n_cuenta;
+        let n_cuenta = req.params.n_cuenta;
+        let update_alumno = req.body;
 
-    //     let update_data = {
+        let update_data = {
 
-    //         nombre = update_alumno.nombre,
-    //         edad = update_alumno.edad,
-    //         genero = update_alumno.genero,
-    //     };
+            nombre : update_alumno.nombre,
+            edad : update_alumno.edad,
+            genero : update_alumno.genero,
+        };
 
-    //     AlumnosModel.findOneAndUpdate({n_cuenta:n_cuenta}, update_data, {new:true}, (err, updateAlumno) => {
-    //         if(err) return res.status(500).json({message:"Error en el proceso de actualización"});
-    //         if(!updateAlumno) return res.status(200).json({message:"No existe el alumno."})
+        Alumnos.findOneAndUpdate({n_cuenta:n_cuenta}, update_data, {new:false}, (err, updateAlumno) => {
+            if(err) return res.status(500).json({message:"Error en el proceso de actualización"});
+            if(!updateAlumno) return res.status(200).json({message:"No existe el alumno."})
 
-    //         return res.status(200).json({
-    //             message: "Alumno actualizado",
-    //             data: updateAlumno
-    //         });
-    //     });
-    // },
+            return res.status(200).json({
+                message: "Alumno actualizado",
+                data: updateAlumno
+            });
+        });
+    },
 
     // delete_alumno: function(req, res) {
 
-    }
+}
 
 module.exports = controller;
